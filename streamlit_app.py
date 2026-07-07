6edcf98b1746ac287e12794213804472a0fe23d1
sumathipichai-dotcom <sumathi.pichai@gmail.com>
2026-07-07 11:36:05 +1000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -52,4 +52,4 @@ if ingredient_list:
 smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/banana")
 
 #st.text(smoothiefroot_response.json())
-sf_df = st.dataframn(data=smoothiefroot_response.json(), use_container_width=True)
+sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
