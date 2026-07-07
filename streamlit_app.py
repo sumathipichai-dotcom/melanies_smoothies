9f958b79b6b7b21b7ce2fa9015b976e05f96ab2a
sumathipichai-dotcom <sumathi.pichai@gmail.com>
2026-07-07 10:53:30 +1000

    Update streamlit_app.py - removed get active session

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,6 @@
 # Co-authored with CoCo
 # Import python packages
 import streamlit as st
-from snowflake.snowpark.context import get_active_session
 from snowflake.snowpark.functions import col
 
 import os
